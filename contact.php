95e90d32bc1f76104ca837e964f3cd67d53f8034
kdejeu <k_dejeu@u.pacific.edu>
2022-11-14 11:08:42 -0800

    Created all the html and css files needed

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/contactstyle.css">`
+    <title>Document</title>
+</head>
+<body>
+<h1>Lets work together</h1>
+<form>
+
+</form>
+
+<div id="socials">
+    <p>adriana@dejeucrew.com</p>
+    <p>@dejeu.crew</p>
+</div>
+
+<div id="calendar">
+    <h2>Check my availibility</h2>
+    <?php ?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dejeu Crew</title>
+    <link rel="stylesheet" href="assets/indexstyle.css">`
+</head>
+<body>
+<nav>
+    <li><a href="#" title="D|C">D|C</a></li>
+    <li><a href="#" title="Home">HOME</a></li>
+    <li><a href="#" title="About">ABOUT</a></li>
+    <li><a href="#" title="Film">FILM</a></li>
+    <li><a href="#" title="Contact">CONTACT</a></li>
+    <li><a href="#" title="Info">INFO</a></li>
+</nav>
+<h1>Dejeu Crew</h1>
+<h2>Film & Photo</h2>
+</body>
+</html>
