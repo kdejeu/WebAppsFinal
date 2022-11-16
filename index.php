efb51c16e6a9bc52271eaee23083cb493fe175eb
kdejeu <k_dejeu@u.pacific.edu>
2022-11-15 23:36:11 -0800

    Added typewriter effect to landing page. changed nav to be static on all other pages besides landing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dejeu Crew</title>
     <link rel="stylesheet" href="assets/landingPage.css">
-    <link rel="stylesheet" href="assets/nav.css">
+    <link rel="stylesheet" href="assets/landing-page-nav.css">
 </head>
 <body>
 <nav>
@@ -29,9 +29,75 @@
     <div class="title">
         <h1>Dejeu Crew</h1><br>
 
-        <h2>Film & Photo</h2>
+        <!--        <h2>Film & Photo</h2>-->
+        <h2>
+            <div class="typewrite" data-period="3000"
+                 data-type='[ "Film & Photo" ]'>
+            </div>
+            <span class="wrap"></span>
+        </h2>
     </div>
 </div>
 
+<script>
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 3000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        // if (this.isDeleting) {
+        //     delta /= 2;
+        // }
+        //
+        // if (!this.isDeleting && this.txt === fullTxt) {
+        //     delta = this.period;
+        //     this.isDeleting = true;
+        // } else if (this.isDeleting && this.txt === '') {
+        //     this.isDeleting = false;
+        //     this.loopNum++;
+        //     delta = 500;
+        // }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function () {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+</script>
 </body>
 </html>
