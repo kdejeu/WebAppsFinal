3ccb5109d50d92f5b8a9202895a9db7d6887a9b7
kdejeu <k_dejeu@u.pacific.edu>
2022-12-05 17:57:31 -0800

    Added all the content for the info page

--- info.php
+++ info.php
@@ -22,5 +22,121 @@
             <li><a href="info.php" title="Info">INFO</a></li>
         </ul>
     </nav>
+    <div class="main-content">
+        <div class="Q&A">
+            <div class="container-element container-element-1 alignwide">
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <h1>Questions</h1>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <h1>Answers</h1>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>How long have I been shooting weddings for?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Over 4+ years professionally, but I've been shooting creatively
+                            ever since I could hold a camera.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>What do you shoot?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Anything and everything. Because I really love photos, and I'm constantly
+                            pushing myself to be creative I'll take on family, grad, studio,
+                            and of course weddings.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Turn Around Rate?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>I know that feeling of anxiously waiting for beautiful pictures.
+                            I do strive to take the time to edit. It takes no more than 2 weeks
+                            for most shoots and around 4 weeks for weddings. I do not procrastinate,
+                            I value quality.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>What if we need to cancel?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>We'll still be friends, things happen. Although the deposit is non-refundable
+                            the rest will be credited. We can always change the date if need be.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Do you offer payment plans?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Yes, we can work out a plan to split payments over a designated period of time.</p>
+                    </div>
+                </div>
+
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Do you retouch?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>I personally do not edit minor retouches, If there are major objects that I think
+                            need to be removed then, yes.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Why the price?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Included in the cost is the years of accrued experience to offer quality service,
+                            as well as the cost of gear, supplies, subscriptions, software, various other expenses,
+                            and my labor shooting and editing.</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Do you travel?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Yes, just ask and we can chat to work something out!</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>Do you send raws?</p>
+                    </div>
+                </div>
+                <div class="grid-item">
+                    <div class="left-centered-element">
+                        <p>No, my work is what I send out. It's my style, so I make sure to only send my
+                            best finished work out. Video is different, we are able to send an uncut roll.</p>
+                    </div>
+                </div>
+            </div>
 </body>
 </html><?php
